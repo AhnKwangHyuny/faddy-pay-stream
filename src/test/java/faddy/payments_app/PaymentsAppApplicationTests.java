3056840e7993b541dc0d92f6b9d0907dc685c626
Ahn Kwang Hyun <ahnkwanghyun@Ahns-MacBook-Air.local>
2025-03-31 17:57:34 +0900

    git connection 테스팅

---
+++ src/test/java/faddy/payments_app/PaymentsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package faddy.payments_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
